890ba03888724cdff7e8a29c44a0784280237a5c
Eduard Alfonso <xsynergy510x@gmail.com>
2015-10-09 12:29:20 -0700

    Build.prop overrides for incrementals Set default build fingerprint to jgedlte

--- cm.mk
+++ cm.mk
@@ -7,3 +7,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_jflte
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jflte \
+    TARGET_DEVICE=jflte \
+    BUILD_FINGERPRINT="samsung/jflte/jgedlte:5.1/LMY47O.S008/150802:user/release-keys" \
+    PRIVATE_BUILD_DESC="jgedlteue-user 5.1 LMY47O.s008 150802 release-keys"
